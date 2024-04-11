3852919438ef9c61dd0ac500aa4847307846ac8b
Flávio Silva <73346782+FuzzyLaDuzzy@users.noreply.github.com>
2024-04-11 18:02:21 +0100

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -23,17 +23,23 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    char pid_command[256];
-    snprintf(pid_command, sizeof(pid_command), "%d ", getpid());
-
+ 
     if(strcmp(argv[1],"execute") == 0){
         if(strcmp(argv[3],"-u") == 0){
-            for (int i = 4; i < argc; i++){
-                write(write_pipe, argv[i], strlen(argv[i]));
-                if(i < argc - 1){
-                    write(write_pipe, " ", 1);
-                }
+            char pid_command[256];
+            snprintf(pid_command, sizeof(pid_command), "%d", getpid());
+
+
+            write(write_pipe,pid_command,strlen(pid_command));
+            char buffer[1024] = {0}; // Buffer to hold the complete command
+            strcat(buffer, pid_command); // Add the pid_command to the buffer
+
+            for (int i = 4; i < argc; i++) {
+                strcat(buffer, " "); // Add a space before each argument
+                strcat(buffer, argv[i]); // Add the argument to the buffer
             }
+
+            write(write_pipe, buffer, strlen(buffer));
         }
         if(strcmp(argv[3],"-p") == 0){
         char *token = strtok(argv[4], "|");
@@ -47,22 +53,40 @@ int main(int argc, char *argv[]) {
     }
 
     if (strcmp(argv[1], "status") == 0) {
+
         // If the user requested the status, write it to the pipe
+        char pipe_name[128];
+        int pid = getpid();
+        char pid2[258];
+        snprintf(pipe_name, sizeof(pipe_name), "/tmp/my_pipe_%d", pid);
+        snprintf(pid2, sizeof(pid2),"%d ", pid);
+        write(write_pipe, pid2, strlen(pid2));
         write(write_pipe, argv[1], strlen(argv[1]));
+        printf("Pipe_name :%s\n", pipe_name);
+        sleep(5);
+        int pipe_read = open(pipe_name, O_RDONLY);
+        if(pipe_read == -1){
+            printf("Pipe_name:%s.\n",pipe_name);
+            perror("Error opening pipe");
+            return 1;
+        }
         char buffer[256];
         while (1) {
-            ssize_t count = read(read_pipe, buffer, sizeof(buffer)-1);
+            ssize_t count = read(pipe_read, buffer, sizeof(buffer)-1);
             if (count > 0) {
                 buffer[count] = '\0';
                 printf("%s", buffer);
                 break;
             } else if (count == 0) {
                 break;
-            } else {
+       while(pipe_read == -1){
+            sleep(1);
+        }     } else {
                 perror("read");
                 return 1;
             }
         }
+        close(pipe_read);
     }
 
     close(write_pipe);

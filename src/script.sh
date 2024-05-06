e59da17b93770c5a2abf338afb9de1526106dc78
Jose Vasconcelos <zevasconcelos6@gmail.com>
2024-05-06 19:23:01 +0100

    Primeiro teste automático feito em bash

---
+++ src/script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Verifica se o Makefile existe
+if [ -f "Makefile" ]; then
+    # Executa o make
+    make
+    sleep 2
+    # Limpa a tela
+    clear
+    gnome-terminal -- "./orchestrator"
+    ./client execute 200 -u "ls -l"
+    echo "Execução da tarefa 1 : ls -l"
+    sleep 2
+    cat "Resultados/output_1.txt"
+    sleep 5
+    # Executa make clean
+    make clean
+    sleep 2
+    clear
+else
+    echo "Erro: Makefile não encontrado."
+    exit 1
+fi
+

d95575829f9ce2e46ecfcb34fbadcb40be5b985b
Jose Vasconcelos <zevasconcelos6@gmail.com>
2024-05-07 11:34:07 +0100

    Tempo de execução adicionado aos testes + cores + novo comando para cada algoritmo

--- src/script_FCFS.sh
+++ src/script_FCFS.sh
@@ -8,10 +8,19 @@ if [ -f "Makefile" ]; then
     # Limpa a tela
     clear
     gnome-terminal -- "./orchestrator" FCFS
-    ./client execute 200 -u "ls -l"
-    echo "Execução da tarefa 1 : ls -l"
-    sleep 2
+    start=$(date +%s%N)
+    ./client execute 40 -u "ls -l"
+    ./client execute 30 -u "ls"
+    end=$(date +%s%N)
+    runtime=$(( (end-start)/1000000 ))
+    echo -e "\n----------\e[32mTeste FCFS\e[0m----------\n"
+    echo -e "\e[32m\nExecução da tarefa 1\e[0m : ls -l\n"
     cat "Resultados/output_1.txt"
+    sleep 1
+    echo -e "\e[32m\nExecução da tarefa 2\e[0m : ls\n"
+    cat "Resultados/output_2.txt"
+    echo -e "\e[32m\nTempo de execução\e[0m : $runtime milissegundos\n"
+    echo -e "\n------------------------------\n"
     sleep 5
     # Executa make clean
     make clean
@@ -21,4 +30,3 @@ else
     echo "Erro: Makefile não encontrado."
     exit 1
 fi
-
